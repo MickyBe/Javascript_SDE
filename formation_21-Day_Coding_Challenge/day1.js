e901d4e7641113256a78f98d5eaacac8b1dbbe1a
MickyBe <bmikiyas@gmail.com>
2023-01-30 14:21:31 +0300

    slow and fast pointer

--- formation_21-Day_Coding_Challenge/day1.js
+++ formation_21-Day_Coding_Challenge/day1.js
@@ -16,15 +16,27 @@ function printLLNode(head){
 
 function deleteMiddleNode(head) {
     /* your code here */
-    head.value = 8888
+    /* slow fast pointer*/
+    console.log("ss",head)
+    let tempHead = new LLNode(Infinity, head);
+    let slow = tempHead;
+    let fast = head?.next;
+    while (fast){
+        slow = slow.next;
+        fast = fast.next?.next;
+    }
+    slow.next = slow.next ? slow.next.next : null;
   }
 
 const test = new LLNode(0);
+
 let point = test;
+
 for(let i=1;i<10;i++){
     point.next = new LLNode(i);
     point = point.next
 }
+
 console.log(test)
 // print all nodes
 printLLNode(test)
