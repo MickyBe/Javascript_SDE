d1be3d3335738257152e4ec620d55cbafad8cbde
MickyBe <bmikiyas@gmail.com>
2023-01-30 20:40:13 +0300

    matrix

---
+++ formation_21-Day_Coding_Challenge/day2.js
@@ -0,0 +1,18 @@
+function spiralTraversal(matrix) {
+    /* your code here */
+    let cs = 0, ce = matrix[0].length, rs = 0, re = matrix.length;
+    let direction = [true, true]; //[col/row, inc/dec];
+    while (ce-cs>0 && ce-cs>0){
+        
+    } 
+    console.log(cs,ce,rs,re)
+  }
+
+const test = [ 
+    [1,  2,  3,  4],
+    [5,  6,  7,  8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]];
+
+    console.log(test);
+    spiralTraversal(test);
